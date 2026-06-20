a021f778512b028ec5fa08447fb29884bcb23696
Santyjl44 <santyjl44@gmail.com>
2026-06-20 16:09:35 -0600

    se añadio la url de la api para el deploy en vercel

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 rm -rf public
 reflex init
-REFLEX_API_URL=
+REFLEX_API_URL=https://mi-stats-spotify-production.up.railway.app
 reflex export --frontend-only
 unzip frontend.zip -d public
 rm -f frontend.zip
