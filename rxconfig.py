14faa370600c3fa3526cc96f6f432e8d73cdc998
Santyjl44 <santyjl44@gmail.com>
2026-06-20 22:59:23 -0600

    se agrego api_url en rxconfig.py

--- rxconfig.py
+++ rxconfig.py
@@ -2,6 +2,7 @@ import reflex as rx
 
 config = rx.Config(
     app_name="my_stats_spotify",
+    api_url="https://mi-stats-spotify-production.up.railway.app",
     plugins=[
         rx.plugins.SitemapPlugin(),
         rx.plugins.TailwindV4Plugin(),
